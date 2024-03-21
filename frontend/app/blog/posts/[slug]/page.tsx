9f2a4e7cdf0359f7bea2a688158705444dbe710e
jackjavi <jackmtembete@gmail.com>
2024-03-21 10:37:00 +0300

    Import file path fixes.

--- frontend/app/blog/posts/[slug]/page.tsx
+++ frontend/app/blog/posts/[slug]/page.tsx
@@ -3,11 +3,11 @@ import { notFound } from "next/navigation";
 import { getAllPosts, getPostBySlug } from "@/lib/api";
 import { CMS_NAME } from "@/lib/constants";
 import markdownToHtml from "@/lib/markdownToHtml";
-import Alert from "@/app/_components/alert";
-import Container from "@/app/_components/container";
-import Header from "@/app/_components/header";
-import { PostBody } from "@/app/_components/post-body";
-import { PostHeader } from "@/app/_components/post-header";
+import Alert from "@/app/blog/_components/alert";
+import Container from "@/app/blog/_components/container";
+import Header from "@/app/blog/_components/header";
+import { PostBody } from "@/app/blog/_components/post-body";
+import { PostHeader } from "@/app/blog/_components/post-header";
 
 export default async function Post({ params }: Params) {
   const post = getPostBySlug(params.slug);
