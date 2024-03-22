572c71f1af206f4be4f5ebe683217bffea54c7b5
jackjavi <jackmtembete@gmail.com>
2024-03-22 15:17:02 +0300

    Cross-origin.

--- backend/app.js
+++ backend/app.js
@@ -16,13 +16,16 @@ const socketHandler = require("./sockets/socketHandler");
 const chatRoutes = require("./routes/chatRoutes");
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://odd2tips.vercel.app/, http://localhost:3000",
 
     credentials: true,
   },
 });
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://odd2tips.vercel.app/, http://localhost:3000"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true);
