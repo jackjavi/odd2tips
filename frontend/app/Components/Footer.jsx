b7ba1c812ba7a60818145adcf486ea0f1358175b
jackjavi <jackmtembete@gmail.com>
2024-03-23 07:35:34 +0300

    Fixed Footer margin-top.

--- frontend/app/Components/Footer.jsx
+++ frontend/app/Components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white mt-8 h-screen md:h-[30vh]">
+    <footer className="bg-gray-800 text-white h-screen md:h-[30vh]">
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* About Section */}
