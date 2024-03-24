bf637bfcbe84b6fadb03490d3932fa252a0040d8
jackjavi <jackmtembete@gmail.com>
2024-03-24 14:43:09 +0300

    Padding.

--- frontend/app/blog/_components/container.tsx
+++ frontend/app/blog/_components/container.tsx
@@ -3,7 +3,11 @@ type Props = {
 };
 
 const Container = ({ children }: Props) => {
-  return <div className="md:container mx-auto px-20 bg-black">{children}</div>;
+  return (
+    <div className="md:container mx-auto md:px-20 px-5 bg-black">
+      {children}
+    </div>
+  );
 };
 
 export default Container;
