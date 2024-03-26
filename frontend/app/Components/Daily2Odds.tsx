d985f84d237c236e6762e6fff6d336d45d32fcd9
jackjavi <jackmtembete@gmail.com>
2024-03-26 15:05:13 +0300

    Game data page.

--- frontend/app/Components/Daily2Odds.tsx
+++ frontend/app/Components/Daily2Odds.tsx
@@ -1,72 +1,68 @@
-import React from "react";
+"use state";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { GameData } from "@/interfaces/gameData";
 import { format } from "date-fns";
 
-const games: GameData[] = [
-  {
-    id: 1,
-    oddTitle: "Match 1",
-    startTime: "2024-03-21T18:00:00Z",
-    homeTeam: "Worthing",
-    awayTeam: "Bath City",
-    prediction: "Total Over 2",
-    odd: 1.26,
-  },
-  {
-    id: 2,
-    oddTitle: "Match 2",
-    startTime: "2024-03-21T18:00:00Z",
-    homeTeam: "Sibenik",
-    awayTeam: "Cibalia",
-    prediction: "Total Over 2",
-    odd: 1.3,
-  },
-  {
-    id: 3,
-    oddTitle: "Match 3",
-    startTime: "2024-03-21T12:30:00Z",
-    homeTeam: "Petrzalka",
-    awayTeam: "Puchov",
-    prediction: "Total Over 2",
-    odd: 1.24,
-  },
-];
-const defaultDate = new Date(2024, 2, 23);
-const totalOdds = games.reduce(
-  (accumulator, game) => accumulator * game.odd,
-  1
-);
-
 const Daily2Odds: React.FC = () => {
+  const [games, setGames] = useState<GameData[]>([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const response = await axios.get<GameData[]>(
+          "http://localhost:8888/api/games"
+        );
+        setGames(response.data);
+      } catch (error) {
+        console.error("Error fetching games data", error);
+      }
+    };
+
+    fetchGames();
+  }, []);
+
+  // Assuming all games have the same predictionType for simplicity
+  const sectionTitle =
+    games.length > 0 ? games[0].predictionType : "Loading...";
+
+  const totalOdds = games.reduce(
+    (accumulator, game) => accumulator * game.odd,
+    1
+  );
+
   return (
     <div className="bg-slate-800 p-4 text-white rounded-lg">
       <div className="mb-4">
-        <h2 className="text-xl font-bold text-center mb-2">DAILY 2 ODDS</h2>
+        <h2 className="text-xl font-bold text-center mb-2">{sectionTitle}</h2>
         <p className="text-center text-sm">
-          {format(defaultDate, "iiii, MMMM do, yyyy")}
+          {format(new Date(), "iiii, MMMM do, yyyy")}
         </p>
       </div>
       {games.map((game) => (
-        <div
-          key={game.id}
-          className="grid grid-cols-12 items-center border-b border-gray-700 p-2 hover:bg-gray-700 transition-colors"
-        >
-          <div className="col-span-3">
-            <span className="text-xs font-medium">
-              {format(new Date(game.startTime), "MMMM d p")}
-            </span>
+        <React.Fragment key={game._id}>
+          <h3 className="text-md font-semibold text-center my-2">
+            {game.gameTitle}
+          </h3>
+          <div className="grid grid-cols-12 items-center border-b border-gray-700 p-2 hover:bg-gray-700 transition-colors">
+            <div className="col-span-3">
+              <span className="text-xs font-medium">
+                {format(new Date(game.startTime), "MMMM d p")}
+              </span>
+            </div>
+            <div className="col-span-6 flex">
+              <span className="font-semibold">{game.homeTeam}</span>
+              <span className="mx-2">vs</span>
+              <span className="font-semibold">{game.awayTeam}</span>
+            </div>
+            <div className="col-span-3 flex justify-end">
+              <span className="w-12 text-center text-sm bg-blue-500 rounded-full py-1">
+                {game.odd.toFixed(2)}
+              </span>
+            </div>
           </div>
-          <div className="col-span-6 flex ">
-            <span className="font-semibold">{game.homeTeam}</span>
-            <span className="mx-2">vs</span>
-            <span className="font-semibold"> {game.awayTeam}</span>
-          </div>
-          <div className="col-span-3 flex justify-end">
-            <span className="w-12 text-center text-sm bg-blue-500 rounded-full py-1">
-              {game.odd.toFixed(2)}
-            </span>
-          </div>
-        </div>
+        </React.Fragment>
       ))}
       <div className="mt-4 p-2 text-right">
         <span className="text-lg font-bold">Total Odds: </span>
