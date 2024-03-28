8d4288215012751abb123f9aff311701be5b928b
jackjavi <jackmtembete@gmail.com>
2024-03-28 16:07:46 +0300

    Remove get req.

--- backend/routes/gameDataRoutes.js
+++ backend/routes/gameDataRoutes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const gameDataController = require("../controllers/gameDataController");
 
 router.post("/gameData", gameDataController.createGameData);
-router.get("/gameData", gameDataController.getGameData);
 
 module.exports = router;
