e2826dd54cb7c7f8c05f1273ebfa33d91e068f3a
jackjavi <jackmtembete@gmail.com>
2024-04-11 01:44:12 +0300

    Increment count

--- backend/controllers/gameDataCollectController.js
+++ backend/controllers/gameDataCollectController.js
@@ -9,18 +9,21 @@ exports.getGameData = async (req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
   try {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
     const updateDoc = {
       $inc: { count: 1 },
       $setOnInsert: {
         ipAddress: ip,
         userAgent: req.headers["user-agent"],
         language: req.headers["accept-language"],
-        date: new Date(),
+        date: today,
       },
     };
 
     const requestCounter = await RequestCount.findOneAndUpdate(
-      { ipAddress: ip, date: { $gte: todayStart, $lte: todayEnd } },
+      { ipAddress: ip, date: today },
       updateDoc,
       { new: true, upsert: true }
     );
