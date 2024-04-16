1559d595fa35d3100b992645df06cb9c1e8535f7
jackjavi <jackmtembete@gmail.com>
2024-04-16 10:17:31 +0300

    INtegrate google font montserrat and playfair_display.

--- frontend/app/Components/AboutSection.tsx
+++ frontend/app/Components/AboutSection.tsx
@@ -5,7 +5,7 @@ const AboutSection: React.FC = () => {
   return (
     <div className="px-4 py-12 h-[75vh] flex flex-col items-center justify-center">
       <div className="text-center mb-8 bg-opacity-20 bg-cover bg-center bg-no-repeat rounded-lg p-6 ">
-        <h1 className="text-5xl md:text-8xl font-bold tracking-wide leading-10 md:pr-8 animate-pulse text-[whitesmoke]">
+        <h1 className="font-playfair text-5xl md:text-8xl font-bold tracking-wide leading-10 md:pr-8 animate-pulse text-[whitesmoke] ">
           Odd2Tips
         </h1>
         {/*<p className="mt-4 text-xl text-[whitesmoke] animate-pulse">
@@ -20,7 +20,7 @@ const AboutSection: React.FC = () => {
           </Link>
   </div>*/}
       </div>
-      <div className="max-w-4xl mx-auto text-center">
+      <div className="max-w-4xl mx-auto text-center font-montserrat">
         <p className="text-lg md:text-xl text-[whitesmoke] mb-4">
           We are a platform that enables tipsters to have a global reach of
           clients and also allows clients to get the best odds from top tipsters
