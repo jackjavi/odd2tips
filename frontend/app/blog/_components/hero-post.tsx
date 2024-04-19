97abbb623155beb08b1f279c2fb292e41386c786
jackjavi <jackmtembete@gmail.com>
2024-04-19 07:21:36 +0300

    If author image for a blog post is null, then default to /logo.png.

--- frontend/app/blog/_components/hero-post.tsx
+++ frontend/app/blog/_components/hero-post.tsx
@@ -18,9 +18,12 @@ export function HeroPost({
   const fullCoverImagePath = coverImagePath.startsWith("http")
     ? coverImagePath
     : `${process.env.NEXT_PUBLIC_BASE_PATH}${coverImagePath}`;
-  const fullAuthorImagePath = authorImagePath.startsWith("http")
-    ? authorImagePath
-    : `${process.env.NEXT_PUBLIC_BASE_PATH}${authorImagePath}`;
+  let fullAuthorImagePath = "/logo.png";
+  if (authorImagePath) {
+    fullAuthorImagePath = authorImagePath.startsWith("http")
+      ? authorImagePath
+      : `${process.env.NEXT_PUBLIC_BASE_PATH}${authorImagePath}`;
+  }
 
   return (
     <section className="hero-post">
