c0eb2df3e99b891cb06a7e89465e177c6d7b8a76
jackjavi <jackmtembete@gmail.com>
2024-04-23 18:28:17 +0300

    Fix controller importing

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/authController");
-const updateProfile = require("../middleware/updateProfile");
 const upload = require("../utils/upload");
 const authenticate = require("../middleware/authenticate");
 
