51964eb58eff0c947e0c7297914c7b189faf9c28
jackjavi <jackmtembete@gmail.com>
2024-04-24 06:14:42 +0300

    Remove logging of cookies

--- backend/controllers/checkAuthController.js
+++ backend/controllers/checkAuthController.js
@@ -1,5 +1,4 @@
 exports.checkAuth = (req, res) => {
-  console.log("req.cookies.token", req.cookies.token, "req.user", req.user);
   res.json({
     token: req.cookies.token,
     isAuthenticated: true,
