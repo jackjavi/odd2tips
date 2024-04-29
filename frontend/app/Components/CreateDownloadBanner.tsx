c722a720c51973718e4d1d4a775ea7396677b42e
jackjavi <jackmtembete@gmail.com>
2024-04-29 08:05:35 +0300

    Color stylings to banner.

--- frontend/app/Components/CreateDownloadBanner.tsx
+++ frontend/app/Components/CreateDownloadBanner.tsx
@@ -6,9 +6,11 @@ const createDownloadBanner: React.FC = () => {
     <section className="bg-teal-600  mx-auto text-white p-6 shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out flex flex-col items-center justify-center">
       <div className="text-center flex flex-col md:flex-row items-center justify-center">
         <div className="font-bold text-lg font-Inter md:text-3xl p-8 text-[#5e17eb]">
-          <span className="text-[whitesmoke]">CREATE</span> CUSTOMIZE AND{" "}
-          <span className="text-[whitesmoke]">DOWNLOAD BETSLIPS</span> FOR FREE
-          WITHOUT REGISTRATION
+          <span className="text-[whitesmoke]">CREATE</span>{" "}
+          <span className="text-slate-700">CUSTOMIZE</span> AND{" "}
+          <span className="text-[whitesmoke]">DOWNLOAD BETSLIPS</span> FOR{" "}
+          <span className="text-slate-700">FREE</span>
+          WITHOUT <span className="text-slate-700">REGISTRATION</span>
         </div>
         <div className="h-[50vh]">
           <Image
