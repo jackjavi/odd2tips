427d8cdae89168abccaaf5be44cac527ecdfaa5a
jackjavi <jackmtembete@gmail.com>
2024-04-29 07:31:00 +0300

    Switch Add and Download components

--- frontend/app/betslip/page.tsx
+++ frontend/app/betslip/page.tsx
@@ -14,11 +14,11 @@ const CreateRoomPage: React.FC = () => {
       <CreateDownloadBanner />
 
       <main className="min-h-screen bg-[whitesmoke] text-teal-500 px-[10%] mx-auto  flex flex-col md:flex-row gap-8">
-        <div className="py-10 md:w-1/3">
-          <AddBetSlipLS />
-        </div>
         <div className="py-10 md:w-1/3 w-full md:flex md:flex-col md:items-center md:justify-center">
           <BetSlipDownload />
+        </div>
+        <div className="py-10 md:w-1/3">
+          <AddBetSlipLS />
         </div>
         <div className="py-10 md:w-1/3 bg-teal-600"></div>
       </main>
