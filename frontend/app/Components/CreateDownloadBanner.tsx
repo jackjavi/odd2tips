25031361ad90652e645f467f655cc960e9110b50
jackjavi <jackmtembete@gmail.com>
2024-04-29 08:31:09 +0300

    REvamp banner and integrate framer-motion library

--- frontend/app/Components/CreateDownloadBanner.tsx
+++ frontend/app/Components/CreateDownloadBanner.tsx
@@ -1,28 +1,57 @@
 import React from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
-const createDownloadBanner: React.FC = () => {
+const DownloadBanner: React.FC = () => {
+  const titleVariants = {
+    initial: { y: -20, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  const infoVariants = {
+    initial: { y: 20, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
   return (
-    <section className="bg-teal-600  mx-auto text-white p-6 shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out flex flex-col items-center justify-center">
-      <div className="text-center flex flex-col md:flex-row items-center justify-center">
-        <div className="font-bold text-lg font-Inter md:text-3xl p-8 text-[#5e17eb]">
-          <span className="text-[whitesmoke]">CREATE</span>{" "}
-          <span className="text-slate-700">CUSTOMIZE</span> AND{" "}
-          <span className="text-[whitesmoke]">DOWNLOAD BETSLIPS</span> FOR{" "}
-          <span className="text-slate-700">FREE </span>
-          WITHOUT <span className="text-slate-700">REGISTRATION</span>
-        </div>
-        <div className="h-[50vh]">
+    <section className="max-w-4xl mx-auto p-6 text-center">
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{ staggerChildren: 0.1 }}
+        className="space-y-4"
+      >
+        <motion.h1
+          variants={titleVariants}
+          transition={{ duration: 0.8 }}
+          className="text-4xl font-bold text-teal-600"
+        >
+          Create & Customize Your Betslips
+        </motion.h1>
+        <motion.p
+          variants={infoVariants}
+          transition={{ duration: 0.8 }}
+          className="text-lg text-teal-600"
+        >
+          Download high-quality betslips tailored to your style without
+          registration.
+        </motion.p>
+        <motion.div
+          variants={infoVariants}
+          transition={{ duration: 0.8 }}
+          className="inline-block"
+        >
           <Image
             src="/assets/betslip/betslip-banner.png"
-            alt="Create, Customize and Download"
-            height={1000}
+            alt="Betslip Banner"
             width={500}
+            height={300}
+            className="rounded-lg shadow-xl"
           />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 };
 
-export default createDownloadBanner;
+export default DownloadBanner;
