8a68e3c158d486a78be26a79d0bc20e3ed8852c3
jackjavi <jackmtembete@gmail.com>
2024-04-30 07:41:11 +0300

    change sort order

--- backend/controllers/getResultsController.js
+++ backend/controllers/getResultsController.js
@@ -2,7 +2,7 @@ const Result = require("../models/Result");
 
 exports.getResults = async (req, res) => {
   try {
-    const results = await Result.find().sort({ date: -1 });
+    const results = await Result.find().sort({ date: 1 });
     res.status(200).json(results);
   } catch (error) {
     res.status(500).send("Server Error: " + error.message);
