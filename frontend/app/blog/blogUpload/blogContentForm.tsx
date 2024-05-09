ee48c2889fd4d3f298141db0ce21cafbbd57d57f
jackjavi <jackmtembete@gmail.com>
2024-05-09 07:38:21 +0300

    blog form

--- frontend/app/blog/blogUpload/blogContentForm.tsx
+++ frontend/app/blog/blogUpload/blogContentForm.tsx
@@ -1,103 +1,47 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { BlogContentFormProps } from "@/interfaces/BlogContent";
-import SimpleMdeReact from "react-simplemde-editor";
+import React, { useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import { markdownToHtml } from "../../utils/markdown";
 
 const BlogContentForm = () => {
   const [contentHtml, setContentHtml] = useState("");
-  const [title, setTitle] = useState("");
-  const [excerpt, setExcerpt] = useState("");
   const [content, setContent] = useState("");
-  const [authorName, setAuthorName] = useState("");
-  const [coverImage, setCoverImage] = useState<File | null>(null);
-  const [authorImage, setAuthorImage] = useState<File | null>(null);
 
-  const handleFormSubmit = () => {
-    const formData = {
-      title,
-      excerpt,
-      content,
-      authorName,
-      coverImage,
-      authorImage,
-    };
-
-    console.log("Form Data:", formData);
-    markdownToHtml(content).then((html: string) => {
-      setContentHtml(html);
-    });
-    console.log("Content HTML:", contentHtml);
-  };
-
-  const handleCoverImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setCoverImage(e.target.files[0]);
-    }
-  };
-
-  const handleAuthorImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setAuthorImage(e.target.files[0]);
-    }
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const html = await markdownToHtml(content);
+    setContentHtml(html);
   };
 
   return (
-    <div>
-      <form
-        onSubmit={handleFormSubmit}
-        className="flex flex-col space-y-4 bg-white p-8 shadow rounded-lg text-black"
-      >
-        <input
-          className="p-2 border border-gray-300 rounded"
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <textarea
-          className="p-2 border border-gray-300 rounded"
-          placeholder="Excerpt"
-          rows={3}
-          value={excerpt}
-          onChange={(e) => setExcerpt(e.target.value)}
-        />
-        <SimpleMdeReact value={content} onChange={setContent} />
-        <input
-          className="p-2 border border-gray-300 rounded"
-          type="text"
-          placeholder="Author Name"
-          value={authorName}
-          onChange={(e) => setAuthorName(e.target.value)}
-        />
-        <div className="file-upload">
-          <label className="file-label">
-            <input
-              className="file-input"
-              type="file"
-              onChange={handleCoverImageChange}
-            />
-            Upload Cover Image
-          </label>
+    <div className="flex flex-col items-center">
+      <form onSubmit={handleFormSubmit} className="w-full max-w-screen-sm">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <SimpleMDE value={content} onChange={setContent} />
+          </div>
         </div>
-        <div className="file-upload">
-          <label className="file-label">
-            <input
-              className="file-input"
-              type="file"
-              onChange={handleAuthorImageChange}
-            />
-            Upload Author Image
-          </label>
+        <div className="flex justify-end">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Convert to HTML
+          </button>
         </div>
-        <button
-          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-          type="submit"
-        >
-          Submit
-        </button>
       </form>
+      {contentHtml && (
+        <div className="w-full max-w-screen-sm mt-8">
+          <h2 className="text-xl font-bold mb-4">HTML Preview:</h2>
+          {contentHtml.length > 0 ? (
+            <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+          ) : (
+            <p>No content</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
