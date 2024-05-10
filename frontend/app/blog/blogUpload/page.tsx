a17d4510e61f4e308fb1e0b0a84a5eb468877aa4
jackjavi <jackmtembete@gmail.com>
2024-05-10 19:42:32 +0300

    remove unused code

--- frontend/app/blog/blogUpload/page.tsx
+++ frontend/app/blog/blogUpload/page.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
-import markdownToHtml from "../../../lib/markdownToHtml";
 import BlogContentForm from "./blogContentForm";
-
-// Dynamically import the SimpleMDE editor with SSR disabled
-const SimpleMdeReact = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-  loading: () => <p>Loading editor...</p>,
-});
 
 const BlogUpload = async () => {
   return (
