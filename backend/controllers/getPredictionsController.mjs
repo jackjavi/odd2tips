977d47f3b59c4534ac39cb819c006c620fdfadf5
jackjavi <jackmtembete@gmail.com>
2024-05-11 08:09:37 +0300

    random predictions -ne draw

--- backend/controllers/getPredictionsController.mjs
+++ backend/controllers/getPredictionsController.mjs
@@ -8,7 +8,10 @@ const getPredictions = async (req, res) => {
     const today = new Date();
     const dateString = formatDate(today);
 
-    const predictions = await Prediction.find({ date: dateString });
+    const predictions = await Prediction.find({
+      date: dateString,
+      prediction: { $ne: "Draw" },
+    });
 
     if (predictions.length < 10) {
       console.error("Not enough predictions available.");
@@ -16,12 +19,13 @@ const getPredictions = async (req, res) => {
     }
 
     const shuffledPredictions = predictions.sort(() => 0.5 - Math.random());
-    const selectedPredictions = shuffledPredictions.slice(0, 12);
+    const selectedPredictions = shuffledPredictions.slice(0, 13);
 
-    const firstRoomPredictions = selectedPredictions.slice(0, 3);
-    const secondRoomPredictions = selectedPredictions.slice(4, 7);
-    const thirdRoomPredictions = selectedPredictions.slice(8, 9);
-    const fourthRoomPredictions = selectedPredictions.slice(10, 12);
+    const firstRoomPredictions = selectedPredictions.slice(0, 2);
+    const secondRoomPredictions = selectedPredictions.slice(3, 6);
+    const thirdRoomPredictions = selectedPredictions.slice(7, 8);
+    const fourthRoomPredictions = selectedPredictions.slice(9, 11);
+    const fifthRoomPredictions = selectedPredictions.slice(12, 13);
 
     const firstRoomId = new mongoose.Types.ObjectId("6618dbf5ad0eed6ed54294b6");
     const secondRoomId = new mongoose.Types.ObjectId(
@@ -31,6 +35,7 @@ const getPredictions = async (req, res) => {
     const fourthRoomId = new mongoose.Types.ObjectId(
       "66374d04c1fc1ffbfedf9f64"
     );
+    const fifthRoomId = new mongoose.Types.ObjectId("663b536c43ce48e99acb6ea3");
 
     const firstRoomEntries = firstRoomPredictions.map((prediction) => ({
       gameTitle: prediction.competitionName,
@@ -92,14 +97,30 @@ const getPredictions = async (req, res) => {
       date: prediction.date,
     }));
 
+    const fifthRoomEntries = fifthRoomPredictions.map((prediction) => ({
+      gameTitle: prediction.competitionName,
+      homeTeam: prediction.homeTeam,
+      awayTeam: prediction.awayTeam,
+      prediction: prediction.prediction,
+      last5home: prediction.last5home,
+      last5away: prediction.last5away,
+      homeOdd: prediction.homeOdd,
+      drawOdd: prediction.drawOdd,
+      awayOdd: prediction.awayOdd,
+      countryName: prediction.countryName,
+      roomId: fifthRoomId,
+      date: prediction.date,
+    }));
+
     await GameData.insertMany(
       firstRoomEntries
         .concat(secondRoomEntries)
         .concat(thirdRoomEntries)
         .concat(fourthRoomEntries)
+        .concat(fifthRoomEntries)
     );
 
-    console.log("GameData entries successfully created for both rooms.");
+    console.log("GameData entries successfully created for all rooms.");
     res.status(200).json({ message: "Predictions allocated to rooms." });
   } catch (error) {
     console.error("Failed to allocate predictions to rooms:", error);
