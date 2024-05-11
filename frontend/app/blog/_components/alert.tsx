7c7a45233183a665cac1c54c86c5adf8dbc114d9
jackjavi <jackmtembete@gmail.com>
2024-05-11 09:48:18 +0300

    fix minor error

--- frontend/app/blog/_components/alert.tsx
+++ frontend/app/blog/_components/alert.tsx
@@ -1,5 +1,4 @@
 import Container from "@/app/blog/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
 import cn from "classnames";
 
 type Props = {
@@ -8,39 +7,14 @@ type Props = {
 
 const Alert = ({ preview }: Props) => {
   return (
-    <div
-      className={cn("border-b", {
-        "bg-neutral-800 border-neutral-800 text-white": preview,
-        "bg-neutral-50 border-neutral-200": !preview,
-      })}
-    >
-      <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This page is a preview.{" "}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-teal-300 duration-200 transition-colors"
-              >
-                Click here
-              </a>{" "}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
-      </Container>
+    <div>
+      <div>
+        {preview ? (
+          <div>
+            This page is a preview. <a>Click here</a> to exit preview mode.
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
