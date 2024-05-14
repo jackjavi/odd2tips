54eda7a327bc19077f6de65d3fa37e9b7a1a2f48
jackjavi <jackmtembete@gmail.com>
2024-05-14 14:40:50 +0300

    Random games for twitter

--- backend/controllers/gameDataCollectController.mjs
+++ backend/controllers/gameDataCollectController.mjs
@@ -60,4 +60,22 @@ const getRandomGameData = async (req, res) => {
   }
 };
 
-export { getGameData, getRandomGameData };
+const getRandomGameforTweets = async (req, res) => {
+  try {
+    const games = await GameData.find({});
+
+    const randomIndex = Math.floor(Math.random() * games.length);
+    const randomGame = games[randomIndex];
+
+    if (randomGame) {
+      res.status(200).json(randomGame);
+    } else {
+      res.status(404).json({ message: "No games found in the database" });
+    }
+  } catch (error) {
+    console.error("Error fetching random game from database:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export { getGameData, getRandomGameData, getRandomGameforTweets };
--- backend/routes/gameDataCollectRoutes.mjs
+++ backend/routes/gameDataCollectRoutes.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getGameData,
   getRandomGameData,
+  getRandomGameforTweets,
 } from "../controllers/gameDataCollectController.mjs";
 import { getGameDataAllDates } from "../controllers/gameDataCollectAllDatesController.mjs";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/gameDataCollect", getGameData);
 router.get("/gameDataCollectAllDates", getGameDataAllDates);
 router.get("/randomGameData", getRandomGameData);
+router.get("/twitter-data", getRandomGameforTweets);
 
 export default router;
