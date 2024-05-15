7de523e9d5d61f06bd4c50b3586cc6ffbddc3936
jackjavi <jackmtembete@gmail.com>
2024-05-15 07:24:12 +0300

    Navbar and Footer

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -6,19 +6,25 @@ import CreateHistory from "./CreateHistory";
 import StorePredictzResults from "./StorePredictzResults";
 import AnalyzeResults from "./AnalyzeResults";
 import AllocateFixtures from "./AllocateFixtures";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const AdminPage: React.FC = () => {
   return (
-    <div>
-      <h1>Admin Page</h1>
-      <ScrapedFixtures />
-      <ScrapedResults />
-      <ScrapePredictions />
-      <AllocateFixtures />
-      <CreateHistory />
-      <StorePredictzResults />
-      <AnalyzeResults />
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <h1>Admin Page</h1>
+        <ScrapedFixtures />
+        <ScrapedResults />
+        <ScrapePredictions />
+        <AllocateFixtures />
+        <CreateHistory />
+        <StorePredictzResults />
+        <AnalyzeResults />
+      </main>
+      <Footer />
+    </>
   );
 };
 
--- frontend/app/blog/blogUpload/page.tsx
+++ frontend/app/blog/blogUpload/page.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "easymde/dist/easymde.min.css";
 import BlogContentForm from "./blogContentForm";
+import Navbar from "@/app/Components/Navbar";
+import Footer from "@/app/Components/Footer";
 
 const BlogUpload = async () => {
   return (
-    <div className="container mx-auto p-4 text-teal-500">
-      <h1 className="text-3xl font-bold text-center mb-6">Upload your Blog</h1>
-      <BlogContentForm />
-    </div>
+    <>
+      <Navbar />
+      <main className="container mx-auto p-4 text-teal-500">
+        <h1 className="text-3xl font-bold text-center py-4 ">
+          Upload your Blog
+        </h1>
+        <BlogContentForm />
+      </main>
+      <Footer />
+    </>
   );
 };
 
