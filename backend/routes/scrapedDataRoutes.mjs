4ed16478c0a88cb8a8ce9b238a2c1500ab7375f3
jackjavi <jackmtembete@gmail.com>
2024-05-17 15:52:21 +0300

    Revert "scrape bbc"
    This reverts commit 1f0fe8504114134d77bea4fd9d945d7e0a9db45a.

--- backend/routes/scrapedDataRoutes.mjs
+++ backend/routes/scrapedDataRoutes.mjs
@@ -4,9 +4,6 @@ import {
   fetchFootballNews,
   fetchFootballResults,
   scrapePredictions,
-  scrapeBBCSport,
-  scrapeBBCArticles,
-  cleanData,
 } from "../controllers/scrapedDataController.mjs";
 import isAppAdmin from "../middleware/isAppAdmin.mjs";
 
@@ -16,8 +13,5 @@ router.get("/results", fetchFootballResults);
 router.get("/fixtures", fetchFootballFixtures);
 router.get("/news", fetchFootballNews);
 router.get("/predictions", scrapePredictions);
-router.get("/bbc-sport", scrapeBBCSport);
-router.get("/bbc-articles", scrapeBBCArticles);
-router.get("/clean", cleanData);
 
 export default router;
