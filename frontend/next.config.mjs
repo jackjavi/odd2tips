bf85e16a36cd4b6b98c874f956863cede9ebc903
jackjavi <jackmtembete@gmail.com>
2024-05-22 15:33:10 +0300

    assets.bundesliga.com config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,13 +4,13 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        //destination: "http://localhost:8888/api/:path*",
-        destination: "https://odd2tips.onrender.com/api/:path*",
+        destination: "http://localhost:8888/api/:path*",
+        //destination: "https://odd2tips.onrender.com/api/:path*",
       },
       {
         source: "/auth/:path*",
-        //destination: "http://localhost:8888/auth/:path*",
-        destination: "https://odd2tips.onrender.com/auth/:path*",
+        destination: "http://localhost:8888/auth/:path*",
+        // destination: "https://odd2tips.onrender.com/auth/:path*",
       },
 
       {
@@ -26,6 +26,7 @@ const nextConfig = {
       "ichef.bbci.co.uk",
       "www.bbc.co.uk",
       "img.legaseriea.it",
+      "assets.bundesliga.com",
     ],
   },
 };
