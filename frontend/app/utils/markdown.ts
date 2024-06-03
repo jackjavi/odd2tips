9b724298750b95d019971055bce460c9be61b8fb
jackjavi <jackmtembete@gmail.com>
2024-06-03 12:30:37 +0300

    xdatas

--- frontend/app/utils/markdown.ts
+++ frontend/app/utils/markdown.ts
@@ -7,6 +7,11 @@ type FormData = {
   fileUrls: string[];
 };
 
+type XData = {
+  markdown: string;
+  fileUrls: string[];
+};
+
 export async function markdownToHtml(markdown: string) {
   const response = await fetch("/api/markdown/convert", {
     method: "POST",
@@ -21,6 +26,18 @@ export async function markdownToHtml(markdown: string) {
 
 export async function uploadBlog(formData: FormData) {
   const response = await fetch("/api/blog/upload", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  const data = await response.json();
+  return data;
+}
+
+export async function uploadX(formData: XData) {
+  const response = await fetch("/api/twitter/xdata", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
