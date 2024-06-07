3ed35e70c3b32c8b817daa8530bd19d2feb993fe
jackjavi <jackmtembete@gmail.com>
2024-06-07 07:23:31 +0300

    cleanup adsterra scripts.

--- frontend/app/(auth)/register/page.jsx
+++ frontend/app/(auth)/register/page.jsx
@@ -188,11 +188,6 @@ export default function Register() {
         ></div>
       </div>
       <Footer />
-      <Script
-        type="text/javascript"
-        async
-        src="//pl23430474.highcpmgate.com/d4/db/06/d4db06bc86d5410193a1ac45bef7482a.js"
-      />
     </div>
   );
 }
