ae42d0112460aadf97fa0e4edf3e396bfbcec8fd
jackjavi <jackmtembete@gmail.com>
2024-06-11 15:58:21 +0300

    tipster competiton announcement.

--- backend/routes/index.mjs
+++ backend/routes/index.mjs
@@ -1,11 +1,13 @@
 import twitterTrendsRoutes from "./scraping/twitterTrendsRoutes.mjs";
 import betHistoryRoutes from "../routes/history/historyRoutes.mjs";
 import xroutes from "./twitter/xroutes.mjs";
+import tipsterEmails from "./tipsterCompRoutes.mjs";
 
 const routes = (app) => {
   app.use("/api/scrapedData", twitterTrendsRoutes);
   app.use("/api/history", betHistoryRoutes);
   app.use("/api/twitter", xroutes);
+  app.use("/api/tipsterEmail", tipsterEmails);
 };
 
 export default routes;
