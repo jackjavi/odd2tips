fd4a97c319f6a8f2884b68de04eccee439edcb46
jackjavi <jackmtembete@gmail.com>
2024-06-21 17:37:37 +0300

    dynamic params

--- frontend/app/admin/GameList.tsx
+++ frontend/app/admin/GameList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PostList from "./PostList";
 import { getAllPosts } from "@/lib/api";
+export const dynamicParams = true;
 
 const GameList: React.FC = async () => {
   const posts = await getAllPosts();
