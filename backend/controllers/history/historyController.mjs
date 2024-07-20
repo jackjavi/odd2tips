3f852b1778337ee3d7c655191fb96300dcecbe42
jackjavi <jackmtembete@gmail.com>
2024-07-20 10:56:00 +0300

    Adsterra is back!

--- backend/controllers/history/historyController.mjs
+++ backend/controllers/history/historyController.mjs
@@ -1,6 +1,79 @@
 import { formatDate } from "../../utils/dateUtils.mjs";
 import History from "../../models/History.mjs";
 import Room from "../../models/Room.mjs";
+import GameData from "../../models/GameData.mjs";
+import GameDataRooms from "../../models/GameDataRooms.mjs";
+
+// update History for each room for each day by checking the status of each game for that day.
+// if all games are WON, then update the status of the room to WON for that day.
+// if any game is LOST, then update the status of the room to LOST for that day.
+// if any game is Pending, then update the status of the room to Pending for that day.
+// only check for today and yesterday games.
+// for today  games -  date;("2024-07-15T19:00"); slice(0, 10) => "2024-07-15"
+// for yesterday games minus 1 from today date.
+
+const updateHistory = async (req, res) => {
+  const today = new Date();
+  const todayDateString = today.toISOString().slice(0, 10);
+
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  const yesterdayDateString = yesterday.toISOString().slice(0, 10);
+
+  try {
+    const rooms = await Room.find();
+
+    for (const room of rooms) {
+      const { _id: roomId } = room;
+
+      const yesterdayGameData = await GameDataRooms.find({
+        date: yesterdayDateString,
+        roomId,
+      });
+
+      const formattedYesterdayDate = formatDate(yesterday);
+
+      const yesterdayStatus = getRoomStatus(yesterdayGameData);
+
+      if (yesterdayStatus === "LOST") {
+        await History.create({
+          roomId,
+          status: "LOST",
+          date: formattedYesterdayDate,
+        });
+      } else if (yesterdayStatus === "WON") {
+        await History.create({
+          roomId,
+          status: "WON",
+          date: formattedYesterdayDate,
+        });
+      } else {
+        await History.create({
+          roomId,
+          status: "Pending",
+          date: formattedYesterdayDate,
+        });
+      }
+    }
+    res.status(200).json({ message: "History updated successfully" });
+  } catch (error) {
+    console.error("Error updating history:", error);
+    res.status(500).json({ message: "Failed to update history" });
+  }
+};
+
+const getRoomStatus = (gameData) => {
+  for (const game of gameData) {
+    if (game.status === "Pending") {
+      return "Pending";
+    } else if (game.status === "LOST") {
+      return "LOST";
+    }
+  }
+
+  return "WON";
+};
 
 const getYesterdayHistory = async (req, res) => {
   let yesterday = new Date();
@@ -51,4 +124,4 @@ const generateCelebratoryMessage = (roomsData) => {
 
 // https://www.odd2tips.com/rooms/league-of-champions-?roomId=663b536c43ce48e99acb6ea3&adminId=661f9e53a8cbd4dee8e6b58a&roomTitle=LEAGUE%20OF%20CHAMPIONS%20%F0%9F%8F%86&roomSlug=league-of-champions-
 
-export { getYesterdayHistory };
+export { getYesterdayHistory, updateHistory };
--- backend/routes/history/historyRoutes.mjs
+++ backend/routes/history/historyRoutes.mjs
@@ -1,8 +1,12 @@
 import express from "express";
-import { getYesterdayHistory } from "../../controllers/history/historyController.mjs";
+import {
+  getYesterdayHistory,
+  updateHistory,
+} from "../../controllers/history/historyController.mjs";
 
 const router = express.Router();
 
 router.get("/get-yesterday-history", getYesterdayHistory);
+router.get("/update-history", updateHistory);
 
 export default router;
