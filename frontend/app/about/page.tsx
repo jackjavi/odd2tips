b48edb2bc61382713fea3aed0c405d3541af42f4
jackjavi <jackmtembete@gmail.com>
2024-09-26 09:00:51 +0300

    Remove adsterra codes

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -48,16 +48,6 @@ const About: React.FC = () => {
         </div>
       </div>
       <Footer />
-      <Script
-        type="text/javascript"
-        async
-        src="//pl23425064.highrevenuenetwork.com/eb/5c/12/eb5c12854223758b1c37d433598047c3.js"
-      />
-      <Script
-        type="text/javascript"
-        async
-        src="//pl23430474.highrevenuenetwork.com/d4/db/06/d4db06bc86d5410193a1ac45bef7482a.js"
-      />
     </div>
   );
 };
